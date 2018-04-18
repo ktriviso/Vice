51d41f64cbba23d66925a96d3304ba2712e54d5c
krista triviso <krista.triviso91@gmail.com>
2018-04-17 20:49:23 -0400

    new api

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const router = require('./routes/dbRoutes.js');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('a66af1cffec146d9a6e58897bd1917ed');
+
+const data = newsapi.v2.everything({
+    q: 'general',
+    sources: 'vice-news',
+    domains: 'https://news.vice.com',
+    from: '2018-01-01',
+    to: '2018-04-17',
+    language: 'en',
+    sortBy: 'publishedAt',
+    page: 20
+}).then(res => {
+    return res
+});
+// app.get('/', (req, res) => {
+//     res.send(data)
+// });
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
