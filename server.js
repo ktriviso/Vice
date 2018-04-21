1b1406d481b3f8883bb76102b1226fd8af3d1a76
krista triviso <krista.triviso91@gmail.com>
2018-04-21 18:15:28 -0400

    .env

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-
 const router = require('./routes/dbRoutes.js');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.set('superSecret', process.env.SERVER_SECRET);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 
-app.user(session({
-    secret: app.get('superSecret'),
-    resave: false,
-    saveUninitalized: false,
+app.use(session({
+  secret: app.get('superSecret'),
+  resave: false,
+  saveUninitialized: false,
 }));
 
 app.use(bodyParser.urlencoded({extended: false}));
